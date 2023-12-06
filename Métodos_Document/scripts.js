75b42700d2df90e385ea220f532572104580770e
SYNAXR <149289092+SYNAXR@users.noreply.github.com>
2023-12-06 16:27:09 -0500

    ambos cambios

--- Métodos_Document/scripts.js
+++ Métodos_Document/scripts.js
@@ -1,3 +1,5 @@
+//finds element by id
+document.getElementById(id)
 /* 
 //finds element by id
 document.getElementById(id)
@@ -7,7 +9,35 @@ document.getElementsByClassName(name)
 
 //finds elements by tag name
 document.getElementsByTagName(name)
- */
+
+let elem = document.getElementById("demo");
+elem.innerHTML = "Hello World!";
+
+let arr = document.getElementsByTagName("p");
+for (let x = 0; x < arr.length; x++) {
+  arr[x].innerHTML = "Hi there";
+}
+/* 
+element.childNodes returns an array of an element's child nodes.
+
+element.firstChild returns the first child node of an element.
+
+element.lastChild returns the last child node of an element.
+
+element.hasChildNodes returns true if an element has any child nodes, otherwise false.
+
+element.nextSibling returns the next node at the same tree level.
+
+element.previousSibling returns the previous node at the same tree level.
+
+element.parentNode returns the parent node of an element. 
+*/
+//finds elements by class name
+document.getElementsByClassName(name)
+
+//finds elements by tag name
+document.getElementsByTagName(name)
+
 /* let elem = document.getElementById("demo");
 elem.innerHTML = "Hello World!";
 
